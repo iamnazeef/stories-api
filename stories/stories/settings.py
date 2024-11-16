618f400f30906cc30fc5b7ea2a524c0537e5b72c
iamnazeef <muhammednazeefedu@gmail.com>
2024-11-17 00:56:22 +0530

    integrate RDS

--- stories/stories/settings.py
+++ stories/stories/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'stories.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('AWS_RDS_NAME'),
+        'USER': os.getenv('AWS_RDS_USER'),
+        'PASSWORD': os.getenv('AWS_RDS_PASSWORD'),
+        'HOST': os.getenv('AWS_RDS_HOST'),
+        'PORT': os.getenv('AWS_RDS_PORT')
     }
 }
 
